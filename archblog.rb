8cedde310c26f4a33a253878ed72fa0ef5ec5733
Reuben <reuben.telushkin@gmail.com>
2015-07-01 15:56:57 -0400

    created views folder and erb files, added styles

--- archblog.rb
+++ archblog.rb
@@ -4,3 +4,20 @@ require './models'
 
 
 set :database, "sqlite3:archblog.sqlite3"
+
+get '/signup' do
+ erb :signup
+end
+
+post '/signup' do
+ confirmation = params[:confirm_password]
+
+ if confirmation == params[:user][:password]
+   @user = User.create(params[:user])
+   "SIGNED UP #{@user.username}"
+ else
+   "Your password & confirmation did not match, try again"
+ end
+
+ 
+end
