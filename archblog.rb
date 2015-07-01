904455d2e261c50e6194c36d02020ea4af4fee04
Reuben <reuben.telushkin@gmail.com>
2015-07-01 17:11:00 -0400

    resolved signup/login bug

--- archblog.rb
+++ archblog.rb
@@ -6,14 +6,20 @@ require './models'
 set :database, "sqlite3:archblog.sqlite3"
 
 get '/' do
- erb :signup
+ erb :login
 end
 
-post '/signup' do
+get '/signup' do
+	erb :signup
+end
+
+post '/login' do
  confirmation = params[:confirm_password]
 
- if confirmation == params[:user][:password]
-   @user = User.create(params[:user])
+ if confirmation == params[:password]
+   username = params[:username]
+   password = params[:password]
+   @user = User.create(username: username, password: password)
    "SIGNED UP #{@user.username}"
  else
    "Your password & confirmation did not match, try again"
@@ -22,10 +28,6 @@ end
 
 get '/landing' do
 	erb :landing
-end
-
-get '/login' do
-	erb :login
 end
 
 get '/profile' do
