b4c2b4e1bdc418ad6fdc3c93c4f66381152aee43
Jacob <Jacobemeier@gmail.com>
2015-07-01 21:37:37 -0400

    created text box and fixed code to link pages

--- archblog.rb
+++ archblog.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
-
+require 'bundler/setup'
+require 'rack-flash'
 
 set :database, "sqlite3:archblog.sqlite3"
+
+enable :sessions
+
+use Rack::Flash, sweep: true
 
 get '/' do
  erb :login
@@ -27,9 +32,35 @@ post '/login' do
 end
 
 get '/landing' do
-	erb :landing
+  # if current_user
+	 erb :landing
+  #  flash[:notice] = "Welcome, #{@user.username}!"
+  # else
+  #   redirect '/login'
+  # end
+end
+
+post '/landing' do
+  @new_post = params["user_post"]
+
 end
 
 get '/profile' do
 	erb :profile
+end
+
+post '/profile' do
+
+end
+
+delete '/profile' do
+
+  session[:user_id]=nil
+  redirect '/login'
+end
+
+def current_user
+  if session[:user_id]
+    User.find session[:user_id]
+  end
 end
